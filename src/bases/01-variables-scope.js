54f01f465a4d1aabb26feb0f9f438e40f7260263
Andres Islas <hir.islas@outlook.com>
2020-11-04 21:07:09 -0600

    Se inicia el curso con repaso de JS para el proyecto

---
+++ src/bases/01-variables-scope.js
@@ -0,0 +1,21 @@
+console.log('hola mundo');
+
+//Variables y contantes
+
+
+let valorDado = 5;
+valorDado = 4;
+
+const nombre = 'Andres';
+const apellido = ' Herrera';
+
+console.log(nombre, apellido, valorDado);
+
+//Scope
+if (true) {
+  const nombre = 'Arturo';
+  console.log(nombre);
+
+};
+
+console.log(nombre);
---
+++ src/bases/02-template-strings.js
@@ -0,0 +1,8 @@
+//template Strings
+
+const nombre = 'Andres';
+const apellido = 'Islas';
+
+const nameComplete = `${nombre} y ${apellido}`;
+
+console.log(nameComplete);
